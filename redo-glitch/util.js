f7db1e319e1a3cf5cf85d2d0bda5624ff2c32f6a
A-username-thats-taken <38640060+A-username-thats-taken@users.noreply.github.com>
2018-04-24 07:38:34 -0400

    Add files via upload

---
+++ redo-glitch/util.js
@@ -0,0 +1,34 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
+const Discord=require('discord.js');
+const client=new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Currently Serving ${client.users.size} users, In ${client.guilds.size} servers.`)
+  client.user.setActivity(`with ${client.users.size} users.`)
+});
+
+
+client.on('message', async msg => {
+  if(msg.author.bot)return;
+  if (msg.content === '>myinfo') {
+    msg.delete(3000)
+    msg.channel.send(`Information about ${msg.author.username} \n \n Name: ${msg.author.username} \n ID: ${msg.author.id} \n Discriminator: ${msg.author.discriminator} \n Nickname (if one exists): ${msg.author.nickname}.`)
+    .catch;
+  }
+  if (msg.content === '>myavatar') {
+   msg.channel.send(`${msg.author.avatarURL} | ID: ${msg.author.avatar}`);
+ }
+});
+
+client.login(process.env.TOKEN);
